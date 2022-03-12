4e26c01a051904addfbfcc60c4d38bea7821b321
DrunkRaccoon98 <32931340+DrunkRaccoon98@users.noreply.github.com>
2022-03-12 13:50:14 +0800

    Cull useless code

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class Monster : MonoBehaviour {
+    Vector2Int gridPosition;
+    
+    
 }
--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -1,23 +1,37 @@
-
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TurnManager : MonoBehaviour {
+
     [Header("Tile Settings")]
     [SerializeField] Vector2 tileSize;
     [SerializeField] Vector2 centerTile;
 
+    [Header("Monsters Management")]
+    public GameObject[] monstersList;
+
+    // Turn Management
+    List<Monster> existingMonsters = new List<Monster>();
+    List<Monster> turnOrder = new List<Monster>();
 
     void Update() {
         if(Input.GetMouseButtonDown(0)) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if(Physics.Raycast(ray, out hit)) {
-                Debug.Log(hit.point);
-                Debug.Log(WorldPosToTile(hit.point));
+                
             }
         }
     }
+
     Vector2Int WorldPosToTile(Vector3 pos) {
         return new Vector2Int(Mathf.RoundToInt((pos.x-centerTile.x)/tileSize.x), Mathf.RoundToInt((pos.z-centerTile.y)/tileSize.y));
+    }
+
+    // Monster Management
+    void SpawnMonster(int monsterIndex) {
+        if(monsterIndex < 0 || monsterIndex >= monstersList.Length) return;
+
+        
     }
 }
